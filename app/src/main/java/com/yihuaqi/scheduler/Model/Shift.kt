9712f564672355ff63447be9a05336215fa6a237
yihuaqi <yihuaqi@bytedance.com>
2017-12-27 22:01:19 +0800

    Add GoodShift and BadShift

--- app/src/main/java/com/yihuaqi/scheduler/Model/Shift.kt
+++ app/src/main/java/com/yihuaqi/scheduler/Model/Shift.kt
@@ -3,12 +3,26 @@ package com.yihuaqi.scheduler.Model
 /**
  * Created by yihuaqi on 12/27/17.
  */
-class Shift(val name: String) {
+sealed class Shift(val name: String) {
     companion object
 }
 
+class GoodShift(n: String): Shift(n) {
+
+}
+
+class BadShift(n: String): Shift(n) {
+
+}
+
 fun Shift.Companion.defaultShift(): List<Shift> {
-    return defaultShiftName().map { Shift(it) }
+    return defaultShiftName().mapIndexed { index, s ->
+        if (index % 2 == 0) {
+            GoodShift(s)
+        } else {
+            BadShift(s)
+        }
+    }
 }
 
 fun Shift.Companion.defaultShiftName(): List<String> {
